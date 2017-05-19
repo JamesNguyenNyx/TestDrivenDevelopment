76bbb2ab15565bfb6d67a400ee711af859320d94
JamesNguyenNyx <jamesnguyen.nyx@gmail.com>
2017-05-19 13:50:21 +0700

    Initial Pod and Setup base Project
    Add some initial for project

---
+++ DailyGoals/DailyGoals/Classes/AppDelegate/AppDelegate.swift
@@ -0,0 +1,36 @@
+//
+//  AppDelegate.swift
+//  DailyGoals
+//
+//  Created by JamesNguyen on 5/19/17.
+//  Copyright © 2017 JamesNguyen. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        self.configureWindow()
+        return true
+    }
+
+}
+
+//MARK: FilePrivate Extension
+fileprivate extension AppDelegate {
+    fileprivate func configureWindow() {
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        if let window = window {
+            let mainViewController = MainViewController()
+            let navi = UINavigationController.init(rootViewController: mainViewController)
+            window.rootViewController = navi
+            window.makeKeyAndVisible()
+        }
+    }
+}
+
---
+++ DailyGoals/DailyGoals/Classes/Controller/MainViewController/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  DailyGoals
+//
+//  Created by JamesNguyen on 5/19/17.
+//  Copyright © 2017 JamesNguyen. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
